79eea95efce294dd96eeb91cc48b23bca86deeb4
AlanPrates <alan_nz@live.com>
2023-12-05 21:33:22 -0800

    CustomDate

--- DataFruta.py
+++ DataFruta.py
@@ -1,76 +1,78 @@
 from abc import ABC, abstractmethod
 
 class CustomDate:
-    def __init__(self, dia=1, mes=1, ano=2000):
-        if dia < 1 or dia > 31:
+
+    def __init__(self, day=1, month=1, year=2000):
+        if day < 1 or day > 31:
             raise ValueError("Dia inválido")
-        if mes < 1 or mes > 12:
+        if month < 1 or month > 12:
             raise ValueError("Mês inválido")
-        if ano < 1900 or ano > 2100:
+        if year < 1900 or year > 2100:
             raise ValueError("Ano inválido")
-        self.__dia = dia
-        self.__mes = mes
-        self.__ano = ano
+        self.__day = day
+        self.__month = month
+        self.__year = year
 
     @property
-    def dia(self):
-        return self.__dia
+    def day(self):
+        return self.__day
 
-    @dia.setter
-    def dia(self, dia):
-        if dia < 1 or dia > 31:
+    @day.setter
+    def day(self, day):
+        if day < 1 or day > 31:
             raise ValueError("Dia inválido")
-        self.__dia = dia
+        self.__day = day
 
     @property
-    def mes(self):
-        return self.__mes
+    def month(self):
+        return self.__month
 
-    @mes.setter
-    def mes(self, mes):
-        if mes < 1 or mes > 12:
+    @month.setter
+    def month(self, month):
+        if month < 1 or month > 12:
             raise ValueError("Mês inválido")
-        self.__mes = mes
+        self.__month = month
 
     @property
-    def ano(self):
-        return self.__ano
+    def year(self):
+        return self.__year
 
-    @ano.setter
-    def ano(self, ano):
-        if ano < 2000 or ano > 2100:
+    @year.setter
+    def year(self, year):
+        if year < 2000 or year > 2100:
             raise ValueError("Ano inválido")
-        self.__ano = ano
+        self.__year = year
 
     def __str__(self):
-        return "{}/{}/{}".format(self.__dia, self.__mes, self.__ano)
+        return "{}/{}/{}".format(self.__day, self.__month, self.__year)
 
-    def __eq__(self, outra_data):
-        return self.__dia == outra_data.__dia and \
-               self.__mes == outra_data.__mes and \
-               self.__ano == outra_data.__ano
+    def __eq__(self, other_date):
+        return self.__day == other_date.__day and \
+               self.__month == other_date.__month and \
+               self.__year == other_date.__year
 
-    def __lt__(self, outra_data):
-        if self.__ano < outra_data.__ano:
+    def __lt__(self, other_date):
+        if self.__year < other_date.__year:
             return True
-        elif self.__ano == outra_data.__ano:
-            if self.__mes < outra_data.__mes:
+        elif self.__year == other_date.__year:
+            if self.__month < other_date.__month:
                 return True
-            elif self.__mes == outra_data.__mes:
-                if self.__dia < outra_data.__dia:
+            elif self.__month == other_date.__month:
+                if self.__day < other_date.__day:
                     return True
         return False
 
-    def __gt__(self, outra_data):
-        if self.__ano > outra_data.__ano:
+    def __gt__(self, other_date):
+        if self.__year > other_date.__year:
             return True
-        elif self.__ano == outra_data.__ano:
-            if self.__mes > outra_data.__mes:
+        elif self.__year == other_date.__year:
+            if self.__month > other_date.__month:
                 return True
-            elif self.__mes == outra_data.__mes:
-                if self.__dia > outra_data.__dia:
+            elif self.__month == other_date.__month:
+                if self.__day > other_date.__day:
                     return True
         return False
+
 
 
 class Data(CustomDate):  # Herda de CustomDate
