78b5511c2419e35f5de466ec4f824410fb8d1c31
AlanPrates <alan_nz@live.com>
2023-12-05 21:22:37 -0800

    CustomDate

--- DataFruta.py
+++ DataFruta.py
@@ -1,104 +1,104 @@
 from abc import ABC, abstractmethod
 
 class CustomDate:
-    def __init__(self, day=1, month=1, year=2000):
-        if day < 1 or day > 31:
+    def __init__(self, dia=1, mes=1, ano=2000):
+        if dia < 1 or dia > 31:
             raise ValueError("Dia inválido")
-        if month < 1 or month > 12:
+        if mes < 1 or mes > 12:
             raise ValueError("Mês inválido")
-        if year < 1900 or year > 2100:
+        if ano < 1900 or ano > 2100:
             raise ValueError("Ano inválido")
-        self.__day = day
-        self.__month = month
-        self.__year = year
+        self.__dia = dia
+        self.__mes = mes
+        self.__ano = ano
 
     @property
-    def day(self):
-        return self.__day
+    def dia(self):
+        return self.__dia
 
-    @day.setter
-    def day(self, day):
-        if day < 1 or day > 31:
+    @dia.setter
+    def dia(self, dia):
+        if dia < 1 or dia > 31:
             raise ValueError("Dia inválido")
-        self.__day = day
+        self.__dia = dia
 
     @property
-    def month(self):
-        return self.__month
+    def mes(self):
+        return self.__mes
 
-    @month.setter
-    def month(self, month):
-        if month < 1 or month > 12:
+    @mes.setter
+    def mes(self, mes):
+        if mes < 1 or mes > 12:
             raise ValueError("Mês inválido")
-        self.__month = month
+        self.__mes = mes
 
     @property
-    def year(self):
-        return self.__year
+    def ano(self):
+        return self.__ano
 
-    @year.setter
-    def year(self, year):
-        if year < 2000 or year > 2100:
+    @ano.setter
+    def ano(self, ano):
+        if ano < 2000 or ano > 2100:
             raise ValueError("Ano inválido")
-        self.__year = year
+        self.__ano = ano
 
     def __str__(self):
-        return "{}/{}/{}".format(self.__day, self.__month, self.__year)
+        return "{}/{}/{}".format(self.__dia, self.__mes, self.__ano)
 
-    def __eq__(self, other_date):
-        return self.__day == other_date.__day and \
-               self.__month == other_date.__month and \
-               self.__year == other_date.__year
+    def __eq__(self, outra_data):
+        return self.__dia == outra_data.__dia and \
+               self.__mes == outra_data.__mes and \
+               self.__ano == outra_data.__ano
 
-    def __lt__(self, other_date):
-        if self.__year < other_date.__year:
+    def __lt__(self, outra_data):
+        if self.__ano < outra_data.__ano:
             return True
-        elif self.__year == other_date.__year:
-            if self.__month < other_date.__month:
+        elif self.__ano == outra_data.__ano:
+            if self.__mes < outra_data.__mes:
                 return True
-            elif self.__month == other_date.__month:
-                if self.__day < other_date.__day:
+            elif self.__mes == outra_data.__mes:
+                if self.__dia < outra_data.__dia:
                     return True
         return False
 
-    def __gt__(self, other_date):
-        if self.__year > other_date.__year:
+    def __gt__(self, outra_data):
+        if self.__ano > outra_data.__ano:
             return True
-        elif self.__year == other_date.__year:
-            if self.__month > other_date.__month:
+        elif self.__ano == outra_data.__ano:
+            if self.__mes > outra_data.__mes:
                 return True
-            elif self.__month == other_date.__month:
-                if self.__day > other_date.__day:
+            elif self.__mes == outra_data.__mes:
+                if self.__dia > outra_data.__dia:
                     return True
         return False
 
 
-class Data(CustomDate):  # Inherit from CustomDate
+class Data(CustomDate):  # Herda de CustomDate
     
     def __str__(self):
-        return "{}/{}/{}".format(self.day, self.month, self.year)
+        return "{}/{}/{}".format(self.dia, self.mes, self.ano)
 
 
 class AnaliseDados(ABC): 
 
     @abstractmethod
-    def __init__(self, tipoDeDados):
-        self.__tipoDeDados = tipoDeDados
+    def __init__(self, tipo_de_dados):
+        self.__tipo_de_dados = tipo_de_dados
 
     @abstractmethod
-    def entradaDeDados(self):
+    def entrada_de_dados(self):
         pass
 
     @abstractmethod
-    def mostraMediana(self):
+    def mostra_mediana(self):
         pass
     
     @abstractmethod
-    def mostraMenor(self):
+    def mostra_menor(self):
         pass
 
     @abstractmethod
-    def mostraMaior(self):
+    def mostra_maior(self):
         pass
 
 
@@ -108,21 +108,24 @@ class ListaNomes(AnaliseDados):
         super().__init__(type("String"))
         self.__lista = []        
 
-    def entradaDeDados(self):
+    def entrada_de_dados(self):
+        # Implemente sua lógica para inserir nomes
         pass
 
-    def mostraMediana(self):
+    def mostra_mediana(self):
+        # Implemente sua lógica para exibir a mediana
         pass    
 
-    def mostraMenor(self):
-
+    def mostra_menor(self):
+        # Implemente sua lógica para exibir o menor elemento
         pass
 
-    def mostraMaior(self):
+    def mostra_maior(self):
+        # Implemente sua lógica para exibir o maior elemento
         pass    
 
     def __str__(self):
-
+        # Implemente sua lógica para exibir a lista
         pass
 	
 class ListaDatas(AnaliseDados):
@@ -131,26 +134,27 @@ class ListaDatas(AnaliseDados):
         super().__init__(type(Data))
         self.__lista = []        
     
-    def entradaDeDados(self):
-
+    def entrada_de_dados(self):
+        # Implemente sua lógica para inserir datas
         pass
     
-    def mostraMediana(self):
-
+    def mostra_mediana(self):
+        # Implemente sua lógica para exibir a mediana
         pass    
      
-    def mostraMenor(self):
-
+    def mostra_menor(self):
+        # Implemente sua lógica para exibir o menor elemento
         pass
     
-    def mostraMaior(self):
-
+    def mostra_maior(self):
+        # Implemente sua lógica para exibir o maior elemento
         pass
     
     def __str__(self):
-
+        # Implemente sua lógica para exibir a lista
         pass
 
+# Modifique as outras classes de maneira semelhante
 
 def main():
     nomes = ListaNomes()
@@ -158,13 +162,13 @@ def main():
     salarios = ListaSalarios()
     idades = ListaIdades()
 
-    listaListas = [nomes, datas, salarios, idades]
+    lista_listas = [nomes, datas, salarios, idades]
 
-    for lista in listaListas:
-        lista.entradaDeDados()
-        lista.mostraMediana()
-        lista.mostraMenor()
-        lista.mostraMaior()
+    for lista in lista_listas:
+        lista.entrada_de_dados()
+        lista.mostra_mediana()
+        lista.mostra_menor()
+        lista.mostra_maior()
         print("___________________")
 
     print("Fim do teste!!!")
